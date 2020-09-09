3007e8fd8db754d0830e7fb43416e5e19e99536b
Shuvranil Mondal <70909882+ShuvranilMondal@users.noreply.github.com>
2020-09-09 09:05:41 +0530

    Update list.py

--- list.py
+++ list.py
@@ -38,3 +38,20 @@ number = [1,3,2,7,4]
 number.sort()
 print(F"{number}")
 
+matrix = [[1,2,3],[4,5,6],[6,7,8]]
+print(matrix[2])                        # output = [6,7,8]
+
+for i in matrix:
+    print(i)                            # use for loop
+
+for sub in matrix:
+    for i in sub:
+        print(i)                        # output = 1,2,3......8 // use to find sub elements of a matrix 
+
+
+n = int(input("Enter : "))
+sub = matrix[n]
+for i in sub:
+    print(i)                             # input = 0 // output = 1,2,3 //
+
+print(matrix[1][0])                        # output = 4 // sub matrix element find 
